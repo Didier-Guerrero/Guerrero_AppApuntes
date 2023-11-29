216ae307f994fe9bbeacaa429c2af62559bee123
Didier Guerrero <didier.guerrero@udla.edu.ec>
2023-11-29 09:48:54 -0500

    Creacion elemento NotePage.xaml modificacion de su codigo, creacion de carpeta views y movilizacion de los respectivos archivos, creacion de carpeta modelos y creacion de los mismos. Adicional se terminaron los pasos del tutorial y se dio la personalizacion al programa.

---
+++ Views/PaginaAcerca.xaml.cs
@@ -0,0 +1,17 @@
+namespace Guerrero_AppApuntes.Views;
+
+public partial class PaginaAcerca : ContentPage
+{
+	public PaginaAcerca()
+	{
+		InitializeComponent();
+	}
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+{
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+}
