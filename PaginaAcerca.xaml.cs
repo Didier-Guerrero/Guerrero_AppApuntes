9e8e0ebf6912072392b6c0bf5047663627876ecf
Didier Guerrero <didier.guerrero@udla.edu.ec>
2023-11-29 08:14:38 -0500

    Adicion Imagenes, modifiacion del codigo AppShell.xaml

--- PaginaAcerca.xaml.cs
+++ PaginaAcerca.xaml.cs
@@ -6,9 +6,9 @@ public partial class PaginaAcerca : ContentPage
 	{
 		InitializeComponent();
 	}
-    private async void LearnMore_Clicked(object sender, EventArgs e)
-    {
-        // Navigate to the specified URL in the system browser.
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
-    }
+	private async void LearnMore_Clicked(object sender, EventArgs e)
+{
+    // Navigate to the specified URL in the system browser.
+    await Launcher.Default.OpenAsync("https://aka.ms/maui");
+}
 }
